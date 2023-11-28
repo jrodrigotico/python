796e30318ed7a389169052cc277e6c1cd7c7166b
João Rodrigo Lemes <joaorodrigo_sp@hotmail.com>
2023-11-27 23:36:50 -0300

    comecei retorno continuo

--- mark_st.py
+++ mark_st.py
@@ -10,18 +10,18 @@ import os
 import plotly.express as px
 import plotly.colors as pcolors
 import plotly.graph_objects as go
+import math
 
-# anotações
+# ---------------- Anotações ---------------- # 
 # arrumar datas nas tabelas
 # ver se consegue tirar o 'empty' da tabela
-
-
-
 # streamlit run markowitz_streamlit.py
-yf.pdr_override() #corrige problemas da bibliotece do pandas_datareader
+
+
 
 # tickers acoes existentes (o ideal seria fazer um web scrapping na B3 ou consumir uma api da B3, pq com um arquivo fica muito travado)
 # https://www.dadosdemercado.com.br/bolsa/acoes , att: Cotações atualizadas no fechamento de 24/11/2023.
+yf.pdr_override() #corrige problemas da bibliotece do pandas_datareader
 acoes = pd.read_excel(os.path.join('C:/Users/Computadores Gamer/OneDrive/Área de Trabalho/python/acoes_listas.xlsx'))['Código']
 
 
@@ -49,10 +49,10 @@ st.subheader('Preço das ações')
 tabela = pd.DataFrame()
 for i in selecionar_acoes:
     tabela[f'{i}'] = round(yf.download(i, start=data_i, end=data_f)['Adj Close'].resample('M').last(),2)
-st.write(tabela.head())
+# st.write(tabela.head())
 
 grafico = px.line(tabela)
-grafico.update_layout(width=800, height=500)
+grafico.update_layout(width=800, height=300)
 st.plotly_chart(grafico)
 
 
@@ -61,9 +61,26 @@ st.subheader('Preço das ações normalizado')
 tabela_norm = pd.DataFrame()
 for i in tabela.columns:
     tabela_norm[f'{i}_normal'] = round(tabela[i] / tabela[i].iloc[0],2) # pega dado da tabela anterior
-st.write(tabela_norm.head())
+# st.write(tabela_norm.head())
     
 grafico2 = px.line(tabela_norm)
-grafico2.update_layout(width=800, height=500)
+grafico2.update_layout(width=800, height=300)
 st.plotly_chart(grafico2)
+
+
+# ---------------- Retornos Contínuos ---------------- #
+# ln(retorno_t / retorno_t-1)
+st.subheader('Retorno Contínuo dos preços')
+tabela_retorno = pd.DataFrame()
+for i in tabela.columns:
+    for z in range(len(tabela)):
+        tabela_retorno[f'{i}_retorn'][z] = math.log(tabela[i][z] / tabela[i][z-1])
+
+st.write(tabela_retorno.head())
+
+# grafico3 = px.line(tabela_norm)
+# grafico3.update_layout(width=800, height=300)
+# st.plotly_chart(grafico3)
+
+
 
