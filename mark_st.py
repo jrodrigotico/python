a832c0d5318da6e1613db5b2cf5fd723c2193299
João Rodrigo Lemes <joaorodrigo_sp@hotmail.com>
2024-01-05 15:27:42 -0300

    procurar carteira para gif

--- mark_st.py
+++ mark_st.py
@@ -94,10 +94,10 @@ if not exibir_introducao:
     st.markdown(''':calendar: O intervalo de tempo considerado está entre 16/01/2013 e 01/11/2023, 
                 sendo possível selecionar a periodicidade dos preços das ações. ''')
     st.text('\n')
+    st.markdown(''':grey_question: Utilizou-se o Subsetor em vez do Segmento de cada ação na B3 para simplificar a seleção das ações.''')
+    st.text('\n')
     st.markdown('''	:flag-br: A taxa livre de risco (*Risk-Free*) escolhida foi a **SELIC**, que será utilizada no cálculo do Índice de Sharpe. Optou-se por 
             utilizar a média aritmética da **SELIC** durante o intervalo de tempo selecionado.''')
-    st.text('\n')
-    st.markdown(''':grey_question: Utilizou-se o Subsetor em vez do Segmento de cada ação na B3 para simplificar a seleção das ações.''')
     st.text('\n')
     st.markdown(''':dollar: No que diz respeito à simulação, não há uma regra definida para o número de portfólios a serem simulados, porém
                 precisa-se de no mínimo duas ações para compor um portfólio.''')
